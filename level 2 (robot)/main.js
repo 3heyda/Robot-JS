659d18fbc719477b447f33025b0a09e5ac217e02
Sheyda Mousavi <121108864+3heyda@users.noreply.github.com>
2023-03-13 14:17:14 -0700

    Add files via upload

---
+++ level 2 (robot)/main.js
@@ -0,0 +1,59 @@
+
+// porsesh va posokh 1
+console.log("------------------روش 1-----------------");
+function resPonse(question) {
+    if (question == "سلام") {
+        console.log("سلاااااام");
+    } else if (question == "خوبی؟") {
+        console.log("مگه دکتری؟");
+    } else if (question == "چه خبر؟") {
+        console.log("سلامتی");
+    } else if (question == "اسمت چیه؟") {
+        console.log("تربچه");
+    } else if (question == "خونت کجاس؟") {
+        console.log("تو باغچه");
+    } else if (question == "چی میخوری؟") {
+        console.log("آلوچه");
+    } else if (question == "کلاس چندمی؟") {
+        console.log("دانشگاه میرم");
+    } else if (question == "هوا چطوره؟") {
+        console.log("نمی دونم");
+    } else {
+        console.log("یکی بود یکی نبود در روزگار امروزی پادشاه وب جاوااسکریپت خود را کتک میزد و از رابی میترسید روزی پادشاه پایتون اعلام جنگ نمود و گفت بابا ولم کن");
+    }
+}
+
+resPonse("سلام")
+resPonse("خوبی؟")
+resPonse('اسمت چیه؟')
+resPonse('خونت کجاس؟')
+resPonse('چی میخوری؟')
+resPonse('کلاس چندمی؟')
+resPonse('هوا چطوره؟')
+resPonse("الووو")
+
+
+
+
+// porsesh va posokh 2
+console.log("------------------روش 2-----------------");
+let question = "خوبی؟"
+if (question == "سلام") {
+    console.log("درود");
+} else if (question == "خوبی؟") {
+    console.log("مگه دکتری؟");
+} else if (question == "چه خبر؟") {
+    console.log("سلامتی");
+} else if (question == "اسمت چیه؟") {
+    console.log("تربچه");
+} else if (question == "خونت کجاس؟") {
+    console.log("تو باغچه");
+} else if (question == "چی میخوری؟") {
+    console.log("آلوچه");
+} else if (question == "کلاس چندمی؟") {
+    console.log("دانشگاه میرم");
+} else if (question == "هوا چطوره؟") {
+    console.log("نمی دونم");
+} else {
+    console.log("کمککککککککک!!!!!!");
+}
